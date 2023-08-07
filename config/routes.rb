b95d1d88380acb20a963c06402e208d9ff52d83f
Daiki Umezawa <137897494+umejaki@users.noreply.github.com>
2023-08-07 12:44:55 +0000

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  devise_for :users
+  root to: 'homes#about'
+  
+  resources :post_images, only: [:new, :create, :index, :show, :destroy]
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get "homes/about" => "homes#about", as: "about"
+  resources :users, only: [:show, :edit, :update]
+end
